ad9c9750647b48474141eecb26b5a9e2f49f4087
Ruber Flores <ruberfc@gmail.com>
2022-07-01 10:35:22 -0500

    'act'

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
+require_once('./app/sunat/lib/phpdotenv/vendor/autoload.php');
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+var_dump($_ENV);
+
+
 echo "Api Sunat";
